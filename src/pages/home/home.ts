800210c2b5d6e4a1e71276bada4ce0877dd195ba
Paulo <pauloafpjunior@hotmail.com>
2018-10-23 14:37:36 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private _peso: number = 1.0;
+  private _altura: number = 1.0;
 
+  constructor(private toastCtrl: ToastController) {
+  }
+
+  set peso(peso: number) {
+    this._peso = peso;
+  }
+
+  get peso() {
+    return this._peso;
+  }
+
+  set altura(altura: number) {
+    this._altura = altura;
+  }
+
+  get altura() {
+    return this._altura;
+  }
+
+  calcularIMC() {
+    let imc = this._peso / (this._altura * this._altura);
+    this.criarToast("IMC = " + new DecimalPipe("en").transform(imc, "1.2-2"));
+  }
+
+  criarToast(msg) {
+    const toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      showCloseButton: true,
+      closeButtonText: "OK"
+    });
+    toast.present();
   }
 
 }
